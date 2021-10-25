852da80293b7097bb1eb0b85636fc7c83864927e
arlez80 <arleyuti@gmail.com>
2021-10-25 19:39:37 +0900

    LICENSEを書き換え

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 """
+    This file is part of Animation Offset Shift.
+
     Copyright (C) 2021 Project Studio Q inc.
 
-    This program is free software; you can redistribute it and/or
+    Animation Offset Shift is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
